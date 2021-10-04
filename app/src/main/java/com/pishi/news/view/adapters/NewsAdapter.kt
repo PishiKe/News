3ca5ae84ade2a03261f9dffc08fcff415e109480
PishiKe <nashpeter33@gmail.com>
2021-10-04 20:05:48 +0300

    ui updates

---
+++ app/src/main/java/com/pishi/news/view/adapters/NewsAdapter.kt
@@ -0,0 +1,11 @@
+package com.pishi.news.view.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter (val fragment : Fragment) : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+
+    class ViewHolder() : RecyclerView.ViewHolder(view.root){
+
+    }
+}
