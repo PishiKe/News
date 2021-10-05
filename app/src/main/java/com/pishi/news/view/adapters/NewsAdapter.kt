23b0a3d67d4bc6ea0731ee2876d00a3428d2387a
PishiKe <nashpeter33@gmail.com>
2021-10-05 05:20:35 +0300

    ui updates

--- app/src/main/java/com/pishi/news/view/adapters/NewsAdapter.kt
+++ app/src/main/java/com/pishi/news/view/adapters/NewsAdapter.kt
@@ -1,11 +1,27 @@
 package com.pishi.news.view.adapters
 
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.pishi.news.databinding.NewsItemBinding
 
 class NewsAdapter (val fragment : Fragment) : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
-    class ViewHolder() : RecyclerView.ViewHolder(view.root){
+    class ViewHolder(view : NewsItemBinding) : RecyclerView.ViewHolder(view.root){
 
+        val ivNews = view.ivNews
+        val tvNews = view.tvNews
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
